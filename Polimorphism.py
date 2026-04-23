03ce84a16db21b81a05479fd501b38db989c0bb5
vivekdeore2525-prog <vivekdeore2525@gmail.com>
2026-04-24 01:18:16 +0530

    Create Polimorphism.py

---
+++ Polimorphism.py
@@ -0,0 +1,17 @@
+class Vehicle:
+    def move(self):
+        print("This vehicle is moving.")
+
+class Car(Vehicle):
+    def move(self):
+        print("Driving on the road")
+
+class Bicycle(Vehicle):
+    def move(self):
+        print("Pedaling on the road")
+
+# Demonstrating polymorphism
+vehicles = [Car(), Bicycle()]
+
+for vehicle in vehicles:
+    vehicle.move()
